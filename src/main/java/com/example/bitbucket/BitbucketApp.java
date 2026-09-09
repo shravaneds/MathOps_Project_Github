beb36734425b76410eb5b645cf079e8a2f65adc0
Shravan <shravan.blr@edstechnologies.com>
2026-09-09 15:29:16 +0530

    version

--- src/main/java/com/example/bitbucket/BitbucketApp.java
+++ src/main/java/com/example/bitbucket/BitbucketApp.java
@@ -8,6 +8,7 @@ public class BitbucketApp {
         System.out.println("6 x 7 = " + mathOps.multiply(6, 7));
         System.out.println("5 / 2 = " + mathOps.divide(5, 2));
         System.out.println("Is 13 prime? " + mathOps.isPrime(13));
+        abc
     }
 
     public String getMessage() {
